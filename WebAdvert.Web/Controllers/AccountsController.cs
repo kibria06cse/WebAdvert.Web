c066cf5b3b991ebe3dce3c920b5bb1d278173797
Abdus Salam Azad <azad.kibria@streamstech.com>
2021-02-16 02:08:25 +0600

    Account controller fixed

--- WebAdvert.Web/Controllers/AccountsController.cs
+++ WebAdvert.Web/Controllers/AccountsController.cs
@@ -1,4 +1,5 @@
-﻿using Amazon.Extensions.CognitoAuthentication;
+﻿using Amazon.AspNetCore.Identity.Cognito;
+using Amazon.Extensions.CognitoAuthentication;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -21,12 +22,26 @@ namespace WebAdvert.Web.Controllers
             this._userManager = userManager;
             this._pool = pool;
         }
-        public async Task<IActionResult> Signup()
+        [HttpGet]
+        public async Task<IActionResult> SignUp()
         {
-            return View();
+            var model = new SignupModel();
+            return View(model);
         }
 
-       [HttpPost]
+        [HttpGet]
+        public async Task<IActionResult> Confirm(ConfirmModel model)
+        {
+            return View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            return View(model);
+        }
+
+        [HttpPost]
         public async Task<IActionResult> Signup(SignupModel model)
         {
             if (ModelState.IsValid)
@@ -48,7 +63,7 @@ namespace WebAdvert.Web.Controllers
                         Code = string.Empty
                     };
 
-                    RedirectToAction("Confirm", confirmModel);
+                    return RedirectToAction("Confirm", confirmModel);
                 }
 
                 foreach (var item in createResult.Errors)
@@ -56,6 +71,54 @@ namespace WebAdvert.Web.Controllers
             }
 
             return View(model);
+        }
+
+        [HttpPost]
+        [ActionName("Confirm")]
+        public async Task<IActionResult> ConfirmPost(ConfirmModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByEmailAsync(model.Email);
+                if (user == null)
+                {
+                    ModelState.AddModelError("NotFound", "A user with given email address was not found.");
+                    return View(model);
+                }
+
+                //  var result = await _userManager.ConfirmEmailAsync(user, model.Code);
+                var result = await ((CognitoUserManager<CognitoUser>)_userManager).ConfirmSignUpAsync(user, model.Code, false);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                foreach (var item in result.Errors) ModelState.AddModelError(item.Code, item.Description);
+                return View(model);
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ActionName("Login")]
+        public async Task<IActionResult> LoginPost(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(model.Email,
+                    model.Password, model.RememberMe, false);
+                if (result.Succeeded)
+                    return RedirectToAction("Index", "Home");
+                ModelState.AddModelError("LoginError", "Email and password do not match");
+            }
+
+            return View("Login", model);
+        }
+
+        public async Task<IActionResult> Signout()
+        {
+            if (User.Identity.IsAuthenticated) await _signInManager.SignOutAsync().ConfigureAwait(false);
+            return RedirectToAction("Login");
         }
     }
 }
